218fbda7f10de5f42d53eabf0002a373b94f3a94
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-09 14:58:28 +0100

    optionals added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,36 +1,32 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
-    # task save that
   end
 
   def create
-    task = Task.new(strong_params)
-    task.save
-    redirect_to task_path(task)
+    @task.save
+    redirect_to task_path(@task)
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(strong_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
+    @task.destroy
     redirect_to tasks_path
   end
 
@@ -38,5 +34,9 @@ class TasksController < ApplicationController
 
   def strong_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
