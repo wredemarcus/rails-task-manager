400c3ba68035f6b68e86f36d49c1f340c40287b9
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-09 12:54:14 +0100

    index and show

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_11_09_104236) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
